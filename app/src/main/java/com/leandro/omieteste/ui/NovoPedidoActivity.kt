2859cc054face77bb437d55e4d7fc31ef6e948c8
Leandro Araujo <lean.ib@hotmail.com>
2025-02-19 17:24:53 -0300

    Criando projeto e primeiras telas

--- app/src/main/java/com/leandro/omieteste/ui/NovoPedidoActivity.kt
+++ app/src/main/java/com/leandro/omieteste/ui/NovoPedidoActivity.kt
@@ -1,10 +1,7 @@
 package com.leandro.omieteste.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
 import com.leandro.omieteste.R
 import com.leandro.omieteste.databinding.ActivityNovoPedidoBinding
